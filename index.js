cdb6bbe6d6180cbbf79386e51e065f98698fbe4a
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-10 01:37:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+  return  x + y
+}
+function subtract(x, y) {
+  return x - y
+}
+function divide(x, y) {
+  return x/y
+}
+
+function multiply(x, y)  {
+
+  return x * y
+}
+function increment(n) {
+var n =5
+return n++
+
+}
